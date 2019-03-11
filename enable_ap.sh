84b09a6c9a2ff0359ade2b4f6cd0a4e94e27a33b
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-03-11 09:12:49 -0500

    Update enable_ap.sh

--- enable_ap.sh
+++ enable_ap.sh
@@ -4,6 +4,57 @@ sleep 3
 
 # enable the AP
 sudo cp config/hostapd /etc/default/hostapd
+
+if [ -e /etc/default/hostapd ]
+then
+    echo "" > /etc/default/hostapd
+    echo "" > /etc/dhcpcd.conf
+    echo "" > /etc/dnsmasq.conf
+    
+    cat <<EOT >> /etc/default/hostapd
+      # Defaults for hostapd initscript
+      #
+      # See /usr/share/doc/hostapd/README.Debian for information about alternative
+      # methods of managing hostapd.
+      #
+      # Uncomment and set DAEMON_CONF to the absolute path of a hostapd configuration
+      # file and hostapd will be started during system boot. An example configuration
+      # file can be found at /usr/share/doc/hostapd/examples/hostapd.conf.gz
+      #
+      #DAEMON_CONF=""
+      #
+      # Additional daemon options to be appended to hostapd command:-
+      #       -d   show more debug messages (-dd for even more)
+      #       -K   include key data in debug messages
+      #       -t   include timestamps in some debug messages
+      #
+      # Note that -B (daemon mode) and -P (pidfile) options are automatically
+      # configured by the init.d script and must not be added to DAEMON_OPTS.
+      #
+      #DAEMON_OPTS=""
+      DAEMON_CONF="/etc/hostapd/hostapd.conf"
+    EOT
+    
+    cat <<EOT >> /etc/dhcpcd.conf
+      
+    EOF
+    
+    cat <<EOT >> /etc/dnsmasq.conf
+      
+    EOF
+    
+    cat <<EOT >> /etc/wpa_supplicant/wpa_supplicant.conf
+      
+    EOF
+else
+    
+fi
+
+echo "" > 
+
+
+
+
 sudo cp config/dhcpcd.conf /etc/dhcpcd.conf
 sudo cp config/dnsmasq.conf /etc/dnsmasq.conf
 
