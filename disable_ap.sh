ef31ffd6fb9eec4a96fbdce5b56f1e488402675d
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-03-14 15:30:31 -0500

    Update disable_ap.sh

--- disable_ap.sh
+++ disable_ap.sh
@@ -3,11 +3,11 @@
 sleep 3
 
 # disable the AP
-sudo cp config/hostapd.disabled /etc/default/hostapd
-sudo cp config/dhcpcd.conf.disabled /etc/dhcpcd.conf
-sudo cp config/dnsmasq.conf.disabled /etc/dnsmasq.conf
+sudo cp main/config/hostapd.disabled /etc/default/hostapd
+sudo cp main/config/dhcpcd.conf.disabled /etc/dhcpcd.conf
+sudo cp main/config/dnsmasq.conf.disabled /etc/dnsmasq.conf
 
 # load wlan configuration
-sudo cp wpa.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp main/wpa.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
 sudo reboot now
