72ea87f2a0f6bdf0f2aaf19ae8b8f9ec159ac3e7
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-03-24 17:59:38 -0500

    Create Docker_Installer.sh

---
+++ main/stage1/Docker_Installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Requirement
+apt update
+apt install -y \
+    apt-transport-https ca-certificates \
+    curl software-properties-common debootstrap qemu-user-static
+
+# Docker CE
+curl -fsSL ./main/Certificates/Docker_ubuntu_gpg | apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt update
+apt install -y docker-ce
+
+# for Docker
+gpasswd -a vagrant docker
