e7773dcba533f90cabdf888ec543bb122c4be669
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-04-19 12:50:54 -0500

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -37,10 +37,31 @@ cat <<EOF > /etc/hostapd/hostapd.conf
 EOF
 
 cat <<EOF > /etc/default/hostapd
-
+    # Defaults for hostapd initscript
+    
+    # See /usr/share/doc/hostapd/README.Debian for information about alternative
+    # methods of managing hostapd.
+    
+    # Uncomment and set DAEMON_CONF to the absolute path of a hostapd configuration
+    # file and hostapd will be started during system boot. An example configuration
+    # file can be found at /usr/share/doc/hostapd/examples/hostapd.conf.gz
+    
+    DAEMON_CONF="/etc/hostapd/hostapd.conf"
+    
+    # Additional daemon options to be appended to hostapd command:-
+    #       -d   show more debug messages (-dd for even more)
+    #       -K   include key data in debug messages
+    #       -t   include timestamps in some debug messages
+    
+    # Note that -B (daemon mode) and -P (pidfile) options are automatically
+    # configured by the init.d script and must not be added to DAEMON_OPTS.
+    
+    # DAEMON_OPTS=""
 EOF
 
-/etc/dnsmasq.conf
+cat <<EOF > /etc/dnsmasq.conf
+
+EOF
 
 # Stage 2
 sudo cp /etc/network/interfaces /etc/network/interfaces-backup
