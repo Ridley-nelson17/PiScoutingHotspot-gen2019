5a0ceedcd5e17f38cc00695ff58332e41b5a105c
Ridley Neslon <43862428+Ridley-nelson17@users.noreply.github.com>
2019-04-19 12:02:26 -0500

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -10,7 +10,8 @@ sudo apt-get install dnsmasq
 sudo systemctl disable hostapd
 sudo systemctl disable dnsmasq
 
-/usr/bin/autohotspotN
+# Copy autohotspotN to /usr/bin/autohotspotN
+sudo cp /etc/autohotspotN* /usr/bin/autohotspotN
 
 cat <<EOF > /etc/hostapd/hostapd.conf
 
@@ -22,21 +23,44 @@ EOF
 
 /etc/dnsmasq.conf
 
-step 2
-
+# Stage 2
 sudo cp /etc/network/interfaces /etc/network/interfaces-backup
 
-/etc/sysctl.conf
+cat <<EOF > /etc/sysctl.conf
 
-/etc/dhcpcd.conf
- nohook wpa_supplicant
+EOF
+
+cat <<EOF > /etc/dhcpcd.conf
+    nohook wpa_supplicant
+EOF
  
- /etc/systemd/system/autohotspot.service
- 
- sudo systemctl enable autohotspot.service
+cat <<EOF > /etc/systemd/system/autohotspot.service
+    [Unit]
+    Description=Automatically generates an internet Hotspot when a valid ssid is not in range
+    After=multi-user.target
+    [Service]
+    Type=oneshot
+    RemainAfterExit=yes
+    ExecStart=/usr/bin/autohotspotN
+    [Install]
+    WantedBy=multi-user.target
+EOF
+
+# start autohotspot
+sudo systemctl enable autohotspot.service
 
 sudo apt-get install iw
 
-
-
 sudo chmod +x /usr/bin/autohotspotN
+
+cat <<EOF > /etc/wpa_supplicant/wpa_supplicant.conf
+    ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+    update_config=1
+    country=GB
+
+    network={
+	    ssid="mySSID1off"
+        psk="myPassword"
+        key_mgmt=WPA-PSK
+    }
+EOF
